42092f3d6277218f1c60b157a0dd46d12fea19e1
amirrexa <amirrezakh123@gmail.com>
2023-09-04 16:17:25 +0330

    Utilizing API, Added Loading State For Inputs

--- src/components/tradebox/tradebox.jsx
+++ src/components/tradebox/tradebox.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Button, Spin, Input, Select } from "antd";
-import { cryptoData } from "../../assets/data/data";
+// import { cryptoData } from "../../assets/data/data";
 import "./tradebox.scss";
 
 export function TradeBox() {
+  const [loading, setLoading] = useState(true);
+  const [cryptoData, setCryptoData] = useState([]);
   const [orderSide, setOrderSide] = useState("buy");
   const [sourceMarket, setSourceMarket] = useState("تومان");
   const [destinationMarket, setDestinationMarket] = useState("تتر");
   const [activeButton, setActiveButton] = useState("buy");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://api.temx.org/api/Market")
+      .then((response) => {
+        if (response.data && response.data.data) {
+          const customData = response.data.data.map((crypto) => ({
+            label: crypto.symbol + " - " + crypto.name,
+            value: crypto.symbol,
+          }));
+          customData.sort((a, b) => a.order - b.order);
+          setCryptoData(customData);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   const handleOnClick = (newOrderSide) => {
     setOrderSide(newOrderSide);
@@ -15,11 +40,19 @@ export function TradeBox() {
   };
 
   const handleSourceMarketSelect = (crypto) => {
-    setSourceMarket(crypto)
+    setSourceMarket(crypto);
+    console.log(
+      "🚀 ~ file: tradebox.jsx:43 ~ handleSourceMarketSelect ~ crypto:",
+      crypto
+    );
   };
 
   const handleDestinationMarketSelect = (crypto) => {
     setDestinationMarket(crypto);
+    console.log(
+      "🚀 ~ file: tradebox.jsx:48 ~ handleDestinationMarketSelect ~ crypto:",
+      crypto
+    );
   };
 
   const renderActionButton = () => {
@@ -27,17 +60,20 @@ export function TradeBox() {
       case "buy":
         return (
           <Button className="buy" id="confirm" onClick={handleBuy}>
-خرید {destinationMarket}          </Button>
+            خرید {destinationMarket}{" "}
+          </Button>
         );
       case "sell":
         return (
           <Button className="sell" id="confirm" onClick={handleSell}>
-فروش {destinationMarket}          </Button>
+            فروش {destinationMarket}{" "}
+          </Button>
         );
       case "swap":
         return (
           <Button className="swap" id="confirm" onClick={handleSwap}>
-تبدیل {sourceMarket} به {destinationMarket}          </Button>
+            تبدیل {sourceMarket} به {destinationMarket}{" "}
+          </Button>
         );
       default:
         return null;
@@ -81,65 +117,67 @@ export function TradeBox() {
             تبدیل{" "}
           </Button>
         </div>
+
         <div className="input-row">
           <div className="label">
             <label>ارز مبداء</label>
           </div>
-          <Input
-            addonBefore="واحد"
-            addonAfter={
-              <Select
-                showSearch
-                placeholder="انتخاب ارز"
-                optionFilterProp="children"
-                onChange={handleSourceMarketSelect}
-                filterOption={(input, option) =>
-                  (option.label ?? "")
-                    .toLowerCase()
-                    .includes(input.toLowerCase())
-                }
-                options={cryptoData.map((crypto) => ({
-                  label: `${crypto.en} - ${crypto.fa}`,
-                  value: crypto.fa,
-                  crypto: crypto,
-                }))}
-                filterSort={(optionA, optionB) =>
-                  (optionA?.label ?? "")
-                    .toLowerCase()
-                    .localeCompare((optionB?.label ?? "").toLowerCase())
-                }
-                style={{ caretColor: "transparent" }}
-                value={sourceMarket}
-              />
-            }
-          />
+          <Spin spinning={loading}>
+            <Input
+              addonBefore="واحد"
+              addonAfter={
+                <Select
+                  showSearch
+                  placeholder="انتخاب ارز"
+                  optionFilterProp="children"
+                  onChange={handleSourceMarketSelect}
+                  filterOption={(input, option) =>
+                    (option.label ?? "")
+                      .toLowerCase()
+                      .includes(input.toLowerCase())
+                  }
+                  options={cryptoData}
+                  filterSort={(optionA, optionB) =>
+                    (optionA?.label ?? "")
+                      .toLowerCase()
+                      .localeCompare((optionB?.label ?? "").toLowerCase())
+                  }
+                  style={{ caretColor: "transparent" }}
+                  value={sourceMarket}
+                  popupMatchSelectWidth
+                  dropdownStyle={{ direction: "rtl" }}
+                />
+              }
+            />
+          </Spin>
         </div>
         <div className="input-row">
           <div className="label">
             <label>ارز مقصد</label>
           </div>
-          <Input
-            addonBefore="واحد"
-            addonAfter={
-              <Select
-                showSearch
-                placeholder="انتخاب ارز"
-                optionFilterProp="children"
-                onChange={handleDestinationMarketSelect}
-                filterOption={(input, option) =>
-                  (option.label ?? "")
-                    .toLowerCase()
-                    .includes(input.toLowerCase())
-                }
-                options={cryptoData.map((crypto) => ({
-                  label: `${crypto.en} - ${crypto.fa}`,
-                  value: crypto.fa,
-                }))}
-                style={{ caretColor: "transparent" }}
-                value={destinationMarket}
-              />
-            }
-          />
+          <Spin spinning={loading}>
+            <Input
+              addonBefore="واحد"
+              addonAfter={
+                <Select
+                  showSearch
+                  placeholder="انتخاب ارز"
+                  optionFilterProp="children"
+                  onChange={handleDestinationMarketSelect}
+                  filterOption={(input, option) =>
+                    (option.label ?? "")
+                      .toLowerCase()
+                      .includes(input.toLowerCase())
+                  }
+                  options={cryptoData}
+                  style={{ caretColor: "transparent" }}
+                  value={destinationMarket}
+                  popupMatchSelectWidth
+                  dropdownStyle={{ direction: "rtl" }}
+                />
+              }
+            />
+          </Spin>
         </div>
         {renderActionButton()}
       </div>
